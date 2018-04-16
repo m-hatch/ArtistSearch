078a5e7397c4912bc681e1cbc8bb6f24cc80fc68
m-hatch <montehatch@gmail.com>
2018-04-15 21:56:27 -0400

    add fetch polyfill

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Promise from 'core-js/es6/promise';
+import assign from 'core-js/fn/object/assign';
+import fetch from 'whatwg-fetch';
 import './styles/main.scss';
 
 // render app
